9e964406d9b5b4781eef92e7f99bc3d741c4da2f
ByronWLitle <bwlitle@amarillocollege.com>
2024-09-16 13:01:34 -0500

    Add project files.

---
+++ Quiz4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Title of program
+            Console.WriteLine("CHECK DUPLICATE");
+            //Space for visibility
+            Console.WriteLine();
+            //Prompts user for string
+            Console.Write("Enter a string: ");
+            //Converts user input to string variable userInput
+            string userInput = Console.ReadLine();
+
+            //Shows user results
+            Console.WriteLine("Result:");
+            //Foreach loop that checks each character in string
+            foreach (char i in userInput)
+            {
+                Console.WriteLine(userInput.IndexOf(i));
+            }
+            //Pauses program for user to view
+            Console.ReadLine();
+        }
+    }
+}
